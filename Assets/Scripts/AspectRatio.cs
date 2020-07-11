0ebfacf60282dcf0b487f352bb1a438bdadf52d5
Nihat Isik <nihat_95@hotmail.it>
2020-07-12 00:38:06 +0200

    First boid impl

---
+++ Assets/Scripts/AspectRatio.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AspectRatio : MonoBehaviour
+{
+    void Start()
+    {
+        Camera.main.aspect = 16f/9f;    
+    }
+}
--- Assets/Scripts/Boid.cs
+++ Assets/Scripts/Boid.cs
@@ -7,12 +7,15 @@ public class Boid : MonoBehaviour
     private Rigidbody2D rb;
     private float velMagnitude;
 
+    private float rayCastLength;
+
     // This list gets filled at every UpdateBoid call
     private List<Boid> neighbors;
 
     public void InitBoid() {
         rb = this.GetComponent<Rigidbody2D>();
         this.velMagnitude = 1.0f;
+        this.rayCastLength = 2.0f;
 
         this.transform.rotation = Quaternion.Euler(0, 0, Random.Range(0f, 360f));
         this.rb.velocity = this.transform.rotation * Vector3.right * velMagnitude;
@@ -40,45 +43,123 @@ public class Boid : MonoBehaviour
             }
         }
 
-        if (neighbors.Count == 0) return;
+        // ==============
+        // Obstacles
+        // ==============
+        Vector3 acceleration = Vector3.zero;
+        Vector3 avoidObstaclesVel = AvoidObstaclesDir() * 2.5f;
+        acceleration += avoidObstaclesVel;
 
-        // 3 RULES
-        // Alignment
-        Vector3 totVel = Vector3.zero;
-        foreach(Boid b in neighbors)
+        // TODO: this makes the boids stick to the edges
+        // If no neighbors then no need to go through rules, just avoid obstacles
+        if (neighbors.Count == 0) //|| Vector3.Magnitude(obstaclesForce) > 3f 
         {
-            // totVel += b.transform.rotation.eulerAngles.z; 
-            Vector3 rb_vel = b.rb.velocity.normalized;
-            totVel += rb_vel;
+            Steer(acceleration);
+            return;
         }
 
-        // Separation 
+        // ============================
+        // 3 RULES
+        // ============================
+        // =====================
+        // Alignment - direction
+        // =====================
+        Vector3 tempAccumulator = Vector3.zero;
+        foreach(Boid b in neighbors)
+        {
+            float dist = Vector3.Magnitude(this.transform.position - b.transform.position);
+            Vector3 rb_vel = b.rb.velocity.normalized / dist;
+            tempAccumulator += rb_vel;
+
+            // Vector3 rb_vel = b.rb.velocity;
+            // tempAccumulator += rb_vel;
+        }
+        acceleration += tempAccumulator / boids.Count * 1.0f;
+
+        // =====================
+        // Separation
+        // ===================== 
         foreach(Boid b in neighbors)
         {
             Vector3 dir = this.transform.position - b.transform.position;
             float dist = Vector3.Magnitude(dir); 
             // Weight the normalized dir by distance (effectively not using sqrt when normalizing -> can be optimized)
-            totVel += dir.normalized / dist;   
+            tempAccumulator += dir.normalized / dist;   
         }
+        acceleration += tempAccumulator * 0.75f;
 
+        // =====================
         // Cohesion 
+        // =====================
         Vector3 centroid = Vector3.zero;
         foreach(Boid b in neighbors)
         {
             centroid += b.transform.position;
         }
         centroid /= neighbors.Count;
-        totVel += centroid - this.transform.position;
+        acceleration += (centroid - this.transform.position) * 0.4f;
 
-        // Average
-        totVel = totVel / 3*neighbors.Count;
-
-        // Clamp speed
-        totVel = Vector3.ClampMagnitude(totVel, 3f);
-
-        Steer(totVel);
-        
+        // =====================
+        // Apply
+        // =====================
+        Steer(acceleration);
         this.neighbors.Clear();
+    }
+
+    public Vector3 AvoidObstaclesDir()
+    {
+        float angleStep = 180f/5;
+        float rotateAngle = 0f;
+        Vector3 prevDir = Vector3.zero;
+        
+        // define state [collided first or not]
+        int firstIteration = 0;
+
+        float minCollDist = Mathf.Infinity;
+
+        for (int i = 0; i < 6; i++)
+        { 
+            Vector3 rayDir = this.transform.TransformDirection(Quaternion.Euler(0, 0, rotateAngle) * Vector3.up);
+            RaycastHit2D hit = Physics2D.Raycast(this.transform.position, rayDir, rayCastLength);
+
+            if (hit.collider != null && !hit.collider.CompareTag("Boid"))
+            {
+                Debug.DrawRay(this.transform.position, rayDir * hit.distance, Color.red, Time.deltaTime);
+             
+                minCollDist = Mathf.Min(minCollDist, hit.distance);
+             
+                if (firstIteration == 0) firstIteration = 1;
+                if (firstIteration == 2)
+                {
+                    Debug.Log("Return left");
+                    return prevDir / (rayCastLength - minCollDist);
+                }
+            }
+            else
+            {
+                Debug.DrawRay(this.transform.position, rayDir * rayCastLength, Color.green, Time.deltaTime);
+                if (firstIteration == 0) firstIteration = 2;
+                if (firstIteration == 1)
+                {
+                    Debug.Log("Return right");
+                    return rayDir / (rayCastLength - minCollDist);
+                }
+            }
+
+            prevDir = rayDir;
+            rotateAngle -= angleStep; 
+        }
+
+        // if all collided (no return until this point & firstIteration=1) go in opposite direction
+        if (firstIteration == 1)
+        {
+            Debug.Log("All collided return");
+            Debug.DrawRay(this.transform.position, Quaternion.Euler(0, 0, 180f) * this.rb.velocity, Color.blue, 1f);
+            return Quaternion.Euler(0, 0, 180f) * this.rb.velocity / (rayCastLength - minCollDist);
+        }
+        
+        // if none collided then return (0,0,0)
+        return Vector3.zero;
     }
 
     /// <summary>
@@ -86,8 +167,10 @@ public class Boid : MonoBehaviour
     /// </summary>
     /// <param name="angle"></param>
     public void Steer(Vector3 vel) {
-        this.rb.velocity = vel;
-        this.transform.rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Atan2(vel.y, vel.x));
+        Vector3 rb_vel = this.rb.velocity;
+        this.rb.velocity = Vector3.ClampMagnitude(rb_vel + vel, 3f);
+
+        this.transform.rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Atan2(this.rb.velocity.y, this.rb.velocity.x));
     }
 
 }
